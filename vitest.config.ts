4103d3637a40e28e5d027f4bf7e225957e3f31aa
Amine Bouzahar <mohamed.amine.bouzahar@gmail.com>
2025-12-03 12:40:55 +0100

    fix: for workflow publish test (#7)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,12 @@
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { defineConfig } from 'vitest/config';
+import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
     plugins: [tsconfigPaths()],
     test: {
         globals: true,
         environment: 'node',
+        exclude: [...configDefaults.exclude, 'dist/**'],
         coverage: {
             provider: 'v8',
             reporter: ['text', 'json', 'html'],
